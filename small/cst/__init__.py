72dc2f32b3a15bc0b0461e9f955d96b0f2ad4ece
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2024-12-18 07:06:20 +0100

    Add parse function

---
+++ small/cst/__init__.py
@@ -0,0 +1,26 @@
+from antlr4 import InputStream, CommonTokenStream
+from antlr4.Recognizer import ConsoleErrorListener
+from small.cst.small.SmallGrammarLexer import SmallGrammarLexer
+from small.cst.small.SmallGrammarParser import SmallGrammarParser
+
+
+def parse(stream: InputStream):
+    lexer = SmallGrammarLexer(stream)
+    tokens = CommonTokenStream(lexer)
+    parser = SmallGrammarParser(tokens)
+
+    parser.removeErrorListeners()
+    errors: list[str] = []
+
+    class ErrorListener(ConsoleErrorListener):
+        def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+            errors.append(f"line {line}:{column} {msg}")
+
+    parser.addErrorListener(ErrorListener())
+
+    cst = parser.program()
+
+    if errors:
+        raise ValueError("\n".join(errors))
+
+    return cst
