756372807fd9764346fd396ae27328ac74994510
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2024-12-28 14:35:37 +0100

    Add more scenes

--- scenes.py
+++ scenes.py
@@ -67,3 +67,81 @@ class NestedIfZeroAnalysisScene(AbstractZeroAnalysisScene):
             return y;
         }
         """
+
+
+class NestedWhileZeroAnalysisScene(AbstractZeroAnalysisScene):
+    title = "Zero Analysis of a Nested While Statement"
+
+    program_string = """
+        function main() {
+            sum = 0;
+            i = 0;
+            while (i < 5) {
+                j = 0;
+                while (j < 5) {
+                    sum = sum + 1;
+                    j = j + 1;
+                }
+                i = i + 1;
+            }
+            return sum;
+        }
+        """
+
+
+class ComplexZeroAnalysisScene(AbstractZeroAnalysisScene):
+    title = "Zero Analysis of a Complex Program"
+
+    program_string = """
+        function main() {
+            y = 5;
+            if (b) {
+                if (y > 0) {
+                    y = x - 2;
+                    x = 2;
+                    while (y > 0) {
+                        y = x - 2;
+                        y = 2 + x;
+                    }
+                    y = 5;
+                } else {
+                    y = 2 + x;
+                    while (y > 0) {
+                        y = x - 2;
+                        y = 2 + x;
+                    }
+                }
+            } else {
+                if (y > 0) {
+                    y = x - 2;
+                    x = 5;
+                    x = 6;
+                    while (y > 0) {
+                        y = x - 2;
+                        y = 2 + x;
+                    }
+                    x = 2;
+                } else {
+                    y = 2 + x;
+                }
+            }
+            if (y > 0) {
+                while (y > 0) {
+                    y = x - 2;
+                    y = 2 + x;
+                }
+                y = x - 2;
+            } else {
+                while (y > 0) {
+                    y = x - 2;
+                    y = 2 + x;
+                    while (y > 0) {
+                        y = x - 2;
+                        y = 2 + x;
+                    }
+                }
+                y = 2 + x;
+            }
+            return y;
+        }
+        """
